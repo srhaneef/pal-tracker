e82f750d857d78fdaf85e98de4ac3fd6fbec8f52
srhaneef <sr.haneef@gmail.com>
2020-07-28 03:53:41 +0000

    Rest endpoints

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -38,6 +38,7 @@ namespace PalTracker
            }
            services.AddSingleton(sp => new WelcomeMessage(message));
            services.AddSingleton(sp=>new CloudFoundryInfo(port,memoryLimit,cfInstanceIndex,cfInsAddr));
+        services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
